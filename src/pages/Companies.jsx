34cf1b0ada376a17a080d351dffa8b7ca538478e
lucky <aakashrecb@gmail.com>
2025-09-07 16:48:14 +0530

    akash add application and companies secation

--- src/pages/Companies.jsx
+++ src/pages/Companies.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import companies from "../api/companies.json"
 
 const Companies = () => {
   return (
-    <div>
-      More Compines
+    <div className="p-6">
+      <h2 className="text-2xl font-bold mb-4">Available Companies & Vacancies</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
+        {companies.map((c, index) => (
+          <div
+            key={index}
+            className="border rounded-2xl p-4 shadow-md hover:shadow-lg transition bg-green-200"
+          >
+            <h3 className="text-xl font-semibold">{c.company}</h3>
+            <p><span className="font-bold">Owner:</span> {c.owner}</p>
+            <p><span className="font-bold">Post:</span> {c.post}</p>
+            <p><span className="font-bold">Last Date:</span> {c.lastDate}</p>
+            <p><span className="font-bold">Seats:</span> {c.seats}</p>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Companies
+export default Companies;
--- src/pages/MyApplication.jsx
+++ src/pages/MyApplication.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import application from "../api/application.json"; 
 
 const MyApplication = () => {
   return (
-    <div>
-      my appalication
+    <div className="p-6">
+      <h2 className="text-2xl font-bold mb-4">My Applications</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {application.map((app, index) => (
+          <div
+            key={index}
+            className="border rounded-2xl p-4 shadow-md hover:shadow-lg transition bg-green-300"
+          >
+            <h3 className="text-xl font-semibold">{app.company}</h3>
+            <p><span className="font-bold">Role:</span> {app.role}</p>
+            <p><span className="font-bold">Date:</span> {app.date}</p>
+            <p><span className="font-bold">Status:</span> {app.status}</p>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MyApplication
+export default MyApplication;
+
